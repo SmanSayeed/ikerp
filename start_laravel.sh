57eeea1f54a212554b3257796ab82d350a619a31
smansayeed <786saadman@gmail.com>
2024-10-17 03:05:43 +0600

    Added sh

--- start_laravel.sh
+++ start_laravel.sh
@@ -7,11 +7,11 @@ PORT="8000"
 QUEUE_TIMEOUT=120
 
 # Start the Laravel application
-nohup php $LARAVEL_DIR/artisan serve --host=$HOST --port=$PORT > $LARAVEL_DIR/serve.log 2>&1 &
+nohup php artisan serve --host=0.0.0.0 --port=8000 > serve.log 2>&1 &
 
 echo "Laravel application started on http://$HOST:$PORT"
 
 # Start the Laravel queue worker
-nohup php $LARAVEL_DIR/artisan queue:work --timeout=$QUEUE_TIMEOUT > $LARAVEL_DIR/queue.log 2>&1 &
+nohup php artisan queue:work --timeout=120 > queue.log 2>&1 &
 
 echo "Laravel queue worker started with a timeout of $QUEUE_TIMEOUT seconds"
